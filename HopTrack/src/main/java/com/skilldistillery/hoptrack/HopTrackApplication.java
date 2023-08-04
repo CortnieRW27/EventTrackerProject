f61739991686d656dd4aa4c892632390ef55c658
Cortnie Barrow <mommy24227@gmail.com>
2023-08-04 19:59:45 -0400

    working springboot

---
+++ HopTrack/src/main/java/com/skilldistillery/hoptrack/HopTrackApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.hoptrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopTrackApplication.class, args);
+	}
+
+}
---
+++ HopTrack/src/test/java/com/skilldistillery/hoptrack/HopTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.hoptrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
